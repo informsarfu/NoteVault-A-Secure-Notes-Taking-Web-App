1b566a19f78f386f6993b3b25bbcce3c4f112d47
informsarfu <informsarfu@gmail.com>
2023-06-25 12:45:10 -0700

    Updated the onclick attribute in button tag

--- website/static/index.js
+++ website/static/index.js
@@ -1,6 +1,6 @@
-function deleteNote(nodeId) {
+function deleteNote(noteId) {
     fetch('/delete-note', 
          {method: 'POST', 
-            body: JSON.stringify({nodeId}),
+            body: JSON.stringify({noteId}),
          }).then((_res) => { window.location.href = "/";});
         }
